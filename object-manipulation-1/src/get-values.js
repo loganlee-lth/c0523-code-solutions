24e54dba9315f77f1c162e0ee21040316e0e57dd
Logan Taehwan Lee <leelogan93@gmail.com>
2023-05-11 20:37:36 +0000

    Completed object-manipulation-1.

---
+++ object-manipulation-1/src/get-values.js
@@ -0,0 +1,9 @@
+/* exported getValues */
+
+function getValues(object) {
+  const valueArr = [];
+  for (const property in object) {
+    valueArr.push(object[property]);
+  }
+  return valueArr;
+}
