a651219976fb55a862b161799ffaaa5d053c4e57
Logan Taehwan Lee <leelogan93@gmail.com>
2023-06-29 12:08:39 +0000

    Completed Phase2.

--- react-design/my-app/src/Indicators.js
+++ react-design/my-app/src/Indicators.js
@@ -1,16 +1,17 @@
-const numberedButtonStyle = {
-  width: '2.5rem',
-  height: '2.5rem',
-};
-
-function Indicators({ items, onCustomClick }) {
-  function handleClick() {}
-
-  const numberedButtons = items.map((item, index) => (
-    <button onClick={handleClick} style={numberedButtonStyle}>{index}</button>
-  ));
-
-  return <div>{numberedButtons}</div>;
+function Indicators({ count, currentIndex, onCustomClick }) {
+  const buttons = [];
+  for (let i = 0; i < count; i++) {
+    if (i === currentIndex) {
+      buttons.push(
+        <button key={i} type="button" onClick={() => onCustomClick(i)} className="">
+          {i}
+        </button>
+      );
+    } else {
+      buttons.push(<button key={i} type="button" onClick={() => onCustomClick(i)}>{i}</button>);
+    }
+  }
+  return <div>{buttons}</div>
 }
 
 export default Indicators;
--- react-design/my-app/src/RotatingBanner.js
+++ react-design/my-app/src/RotatingBanner.js
@@ -3,13 +3,16 @@ import Banner from './Banner';
 import PrevButton from './PrevButton';
 import NextButton from './NextButton';
 import Indicators from './Indicators';
+import { useState } from 'react';
 
 export default function RotatingBanner({ items }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   return (
     <div>
-      <Banner item={items[0]} />
+      <Banner item={items[currentIndex]} />
       <PrevButton />
-      <Indicators items={items} />
+      <Indicators count={items.length} current={currentIndex}/>
       <NextButton />
     </div>
   );
