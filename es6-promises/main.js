36fa7ada75dad36790a7ae7355547fb7d460fdb7
Logan Taehwan Lee <leelogan93@gmail.com>
2023-07-06 21:55:30 +0000

    Completed es6-promises.

--- es6-promises/main.js
+++ es6-promises/main.js
@@ -1,10 +1,7 @@
 import takeAChance from './take-a-chance.js';
 
 const promise = takeAChance('Logan');
+
 promise
-  .then((value) => {
-    console.log(value);
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
+  .then((message) => console.log(message))
+  .catch((error) => console.log(error.message));
