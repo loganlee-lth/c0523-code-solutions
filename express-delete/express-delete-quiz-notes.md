fb211ec66a3e8bac83019996822dec52be81e480
Logan Taehwan Lee <leelogan93@gmail.com>
2023-07-10 23:49:18 +0000

    Completed express-delete.

---
+++ express-delete/express-delete-quiz-notes.md
@@ -0,0 +1,34 @@
+# express-delete-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What is the significance of an HTTP request's method?
+HTTP methods are used to indicate the desired action to be performed on the specified path in an HTTP request. Although there are no set rules, there are general standards to enable communication with a clear intent
+
+## Notes
+
+All student notes should be written here.
+
+
+How to write `Code Examples` in markdown
+
+for JS:
+```javascript
+const data = "Howdy"
+```
+
+for HTML:
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+```css
+div {
+  width:100%
+}
+```
---
+++ express-delete/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92,
+  },
+};
+
+const app = express();
+
+app.get('/api/grades', (req, res) => {
+  const gradesArr = [];
+  for (const grade in grades) {
+    gradesArr.push(grades[grade]);
+  }
+  res.json(gradesArr);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  const id = req.params.id;
+  if (!grades[id]) {
+    res.status(404).send(`Invalid ID: ${id}`);
+  } else {
+    delete grades[id];
+    res.sendStatus(204);
+  }
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080!');
+});
