d618e991c9565ea27e59b28e75a8015dea57002e
Logan Taehwan Lee <leelogan93@gmail.com>
2023-07-20 23:20:40 +0000

    Add error handling.

--- api-authorization/server/lib/authorization-middleware.js
+++ api-authorization/server/lib/authorization-middleware.js
@@ -5,6 +5,9 @@ import { ClientError } from './client-error.js';
 export function authMiddleware(req, res, next) {
   /* your code here */
   const auth = req.headers.authorization;
+  if (!auth) {
+    throw new ClientError(401, 'authentication required');
+  }
   const token = auth.split('Bearer ')[1];
   if (!token) {
     throw new ClientError(401, 'authentication required');
